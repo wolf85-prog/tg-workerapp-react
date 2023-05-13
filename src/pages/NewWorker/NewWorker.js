c75c4259e1a04d1121d1c8b5f5bfd619adad7347
seregey gornih <you@example.com>
2023-05-13 15:29:27 +0300

    update new

---
+++ src/pages/NewWorker/NewWorker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+
+const NewWorker = () => {
+    return (
+        <div className="App">
+            <Header header={{title: 'Новый специалист', icon: 'false'}}/>
+            
+        </div>
+    );
+};
+
+export default NewWorker;
