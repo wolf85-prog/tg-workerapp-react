31150fe5b7d5a93d6979036755e54e51f98ae7dd
Sergey <seregaland@yandex.ru>
2023-09-20 15:53:05 +0300

    fix

--- src/pages/LoadPage/LoadPage.js
+++ src/pages/LoadPage/LoadPage.js
@@ -22,10 +22,10 @@ const LoadPage = () => {
         setTimeout(() =>  setShowLogo(true), 1000) //U.L.E.Y.
         setTimeout(() =>  setShowLogo2(true), 3000) // WORKHUB
 
-        setTimeout(() =>  setShowLogo(false), 5000)
-        setTimeout(() =>  setShowLogo2(false), 5000)
+        //setTimeout(() =>  setShowLogo(false), 5000)
+        //setTimeout(() =>  setShowLogo2(false), 5000)
 
-       setTimeout(() =>  navigate("/add-worker"), 6000)
+       //setTimeout(() =>  navigate("/add-worker"), 6000)
     }, []);
 
 
@@ -34,10 +34,10 @@ const LoadPage = () => {
 
             <img src={Fon} alt='' className='fon-style'/>
             <img src={FonGrad} alt='' className='fon-style2'/>
-            <img src={Logo} alt='' className='fon-style3' style={{display: showLogo ? "block": "none"}}/>
-            <img src={Logo2} alt='' className='fon-style4' style={{display: showLogo2 ? "block": "none"}}/>
-            {/* <img src={Logo} alt='' className='fon-style3' />
-            <img src={Logo2} alt='' className='fon-style4' /> */}
+            {/* <img src={Logo} alt='' className='fon-style3' style={{display: showLogo ? "block": "none"}}/>
+            <img src={Logo2} alt='' className='fon-style4' style={{display: showLogo2 ? "block": "none"}}/> */}
+            <img src={Logo} alt='' className='fon-style3' />
+            <img src={Logo2} alt='' className='fon-style4' />
             
         </div>
     );
