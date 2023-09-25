47f3eae1119873cb71f46cf003de199e98ea2766
Sergey <seregaland@yandex.ru>
2023-09-25 09:57:58 +0300

    fix spec

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -24,7 +24,7 @@ const ProfilePage = () => {
     useEffect(() => {
 
         const fetchData = async() => {
-            const worker = await getWorkerId(user?.id)
+            const worker = await getWorkerId(user?.id) //user?.id
 
             console.log("worker: ", worker)
 
@@ -52,14 +52,15 @@ const ProfilePage = () => {
                     <li><div className="bullet-title">Имя</div>{workerhub[0]?.fio.split(' ')[1]}</li>
                     <li><div className="bullet-title">Отчество</div>{workerhub[0]?.fio.split(' ')[2]}</li>
                     <li><div className="bullet-title">Город</div>{workerhub[0]?.city}</li>
-                    <li><div className="bullet-title">Специальность</div><ul className="spec">{workerhub[0]?.spec.map((worker, index) => index < 5 ? <li key={worker.id}>{worker.name}</li> : '' )}</ul>                                                       
+                    <li><div className="bullet-title">Специальность</div>              
+                        <table className="table-noborder">{workerhub[0]?.spec.map((worker, index) => index < 5 ? <tr><td key={worker.id}>{worker.name}</td></tr> : '' )}</table>
                     </li>
                     <li><div className="bullet-title">Рейтинг</div>
                         &#9733;&#9733;&#9733;&#9733;&#9733;  
                     </li>
                     <li><div className="bullet-title">Компетенции</div></li>
                     <li><div className="bullet-title">Проекты с U.L.E.Y</div>{workerhub[0]?.rank}</li>
-                    <li><div className="bullet-title">Замечания</div>{workerhub[0]?.comteg.map(item=>item.name).join(' ')}</li>
+                    {/* <li><div className="bullet-title">Замечания</div>{workerhub[0]?.comteg.map(item=>item.name).join(' ')}</li> */}
                     <li><div className="bullet-title">Мерч</div><input className='input-style' id="html" type="checkbox" checked="checked"></input></li>
                     <li><div className="bullet-title"></div>{workerhub[0]?.merch.map(item=>item.name).join(' ')}</li>
                 </ol>
