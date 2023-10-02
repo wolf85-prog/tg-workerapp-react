4644f5bc265c138653c37d4ca3d74ffa43982f90
seregey gornih <you@example.com>
2023-10-02 20:03:37 +0300

    fix

--- src/pages/LoadPage/LoadPage.js
+++ src/pages/LoadPage/LoadPage.js
@@ -22,10 +22,10 @@ const LoadPage = () => {
         setTimeout(() =>  setShowLogo(true), 1000) //U.L.E.Y.
         setTimeout(() =>  setShowLogo2(true), 2000) // WORKHUB
 
-        setTimeout(() =>  setShowLogo(false), 8000)
-        setTimeout(() =>  setShowLogo2(false), 8000)
+        setTimeout(() =>  setShowLogo(false), 7000)
+        setTimeout(() =>  setShowLogo2(false), 7000)
 
-        setTimeout(() =>  navigate("/hello"), 8000)
+        setTimeout(() =>  navigate("/hello"), 7000)
     }, []);
 
 
