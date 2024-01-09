33c7e8a4320b2e5614d01be924515732d0a47fd3
seregey gornih <you@example.com>
2024-01-09 18:12:57 +0300

    fix

--- src/components/WorkerList2/WorkerList2.jsx
+++ src/components/WorkerList2/WorkerList2.jsx
@@ -7,7 +7,7 @@ const WorkerList2 = ({workers}) => {
         <div style={{marginLeft: '25px', overflow: 'hidden'}}>
            <ul style={{textAlign: 'left', marginBottom: '15px'}}> 
             {workers.slice(0).reverse().map((worker, index) =>               
-                <li style={{height: '25px'}}>{worker.spec}</li>             
+                <li style={{height: '25px', display: 'flex'}}><p style={{fontSize: '5px', marginRight: '10px'}}>&#x25CF;</p>{worker.spec}</li>             
             )}
             </ul> 
         </div>
