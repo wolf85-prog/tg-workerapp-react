b240fa3b5dce8f40d0c3cfea407c0ed1eb054603
seregey gornih <you@example.com>
2024-01-13 11:28:27 +0300

    fix

--- src/components/UI/RangeSlider/RangeSlider.jsx
+++ src/components/UI/RangeSlider/RangeSlider.jsx
@@ -30,6 +30,37 @@ const RangeSlider = ({min, max, value, step}) => {
         handleSliderInput();
     }, [sliderRef])
 
+    /**
+     * Determine the mobile operating system.
+     * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+     *
+     * @returns {String}
+     */
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+
+        return "unknown";
+    }
+
+    useEffect(()=> {
+        //console.log(getMobileOperatingSystem())
+        alert(getMobileOperatingSystem())
+    },[])
+
     return (
         <div className='range-slider'>
                 <div className="shkala">
