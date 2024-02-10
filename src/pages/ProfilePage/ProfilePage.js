5a31b82c49a40201a2e3d83a5367ec32b549f917
seregey gornih <you@example.com>
2024-02-10 11:23:10 +0300

    fix

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -798,7 +798,7 @@ useEffect(()=> {
                                 {workerhub[0]?.spec.length > 0 ? workerhub[0]?.spec.map((worker, index) => index < 8 && worker.name !== 'Blacklist' 
                                 ?   <li className="bullet-title">{worker.name}  {index === workerhub[0]?.spec.length-1 && <img src={Edit} onClick={clickAddSpec} alt='' style={{marginLeft: '20px', width: '12px'}}/> }</li>
                                 : '')
-                                : <><li className="bullet-title" style={{color: 'red', fontWeight: 'bold'}}>Добавь свою специальность</li><li> <img src={Edit2} onClick={clickAddSpec} alt='' style={{marginLeft: '100px', width: '25px'}}/> </li></> }
+                                : <><li className="bullet-title" style={{color: 'red', fontWeight: 'bold'}}>Добавь свою специальность</li><li> <img src={Edit2} onClick={clickAddSpec} alt='' style={{marginLeft: '90px', width: '25px'}}/> </li></> }
                             </ul>   
                         </div>     
                     </div>
