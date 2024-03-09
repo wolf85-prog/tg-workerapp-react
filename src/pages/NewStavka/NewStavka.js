633f15b83df2a40551262d3fcebea4a714a4a32b
seregey gornih <you@example.com>
2024-03-09 19:35:43 +0300

    fix stavka

--- src/pages/NewStavka/NewStavka.js
+++ src/pages/NewStavka/NewStavka.js
@@ -6,21 +6,14 @@ import MyButton from "../../components/UI/MyButton/MyButton";
 import './NewStavka.css';
 import CurrencyInput from './../../common/CurrencyInput'
 
-import BlackFon from "../../image/background/Background_black_600X800.png";
-import Fon from "../../image/icons/U.L.E.Y_triangle4_main2.png";
-import FonGradTop from "../../image/layers/upper_red_corner_menu2.png";
-import FonGradBottom from "../../image/layers/lower_blue_corner_menu.png";
-
-import btnMenu from "../../image/layers/icon_menu.png";
-import smallMenu from "../../image/layers/logo_04_light.png"
-
-import enterSumma from "../../image/buttons/enter_summa.png"
-import buttonSend from "../../image/buttons/button_send_predlog.png"
+import BlackFon from "../../image/new/fon_grad.svg";
 
 import TextField from '@mui/material/TextField';
 import { alpha, styled } from '@mui/material/styles';
 
 import InputMask from 'react-input-mask';
+
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 
 
 //import TextField from '@mui/material/TextField';
@@ -42,6 +35,9 @@ const NewStavka = () => {
     const [summaStavki, setSummaStavki] = useState()
     const [pretendentId, setPretendentId] = useState()
 
+    const { height, width } = useWindowDimensions();
+    const [widthD, setWidthD] = useState(0)
+
     //const pretId = props.match.params.id
 
 
@@ -52,11 +48,8 @@ const NewStavka = () => {
 
     // при первой загрузке приложения выполнится код ниже
     useEffect(() => {
-        setTimeout(() =>  setShowGrad2(true), 500) // градиент низ
-        setTimeout(() =>  setShowGrad(true), 4500) //градиент верх 
-
         setPretendentId(id)
-    })
+    }, [])
 
 
     const changeSummaStavki = (e) => {
@@ -119,32 +112,36 @@ const NewStavka = () => {
 
             {/* темный фон */}
             <img src={BlackFon} alt='' className='fon-black' />
-            
-            <div style={{display: 'flex', height: '100vh', position: 'fixed', right: '0'}}>
-                <img src={Fon} alt='' className='fon-style-full' />
-            </div>
+            <img src={BlackFon} alt='' className='fon-black' style={{left: `${widthD}px`, zIndex: '1'}} />
 
-            <img src={FonGradTop} alt='' className='fon-style-menu1' style={{visibility: showGrad ? "visible": "hidden"}}/>
-            <img src={FonGradBottom} alt='' className='fon-style-menu2' style={{visibility: showGrad2 ? "visible": "hidden"}}/>
 
-            <div style={{display: 'flex', height: '100vh', position: 'fixed', width: '100%'}}>
+            {/* <img src={FonGradTop} alt='' className='fon-style-menu1' style={{visibility: showGrad ? "visible": "hidden"}}/>
+            <img src={FonGradBottom} alt='' className='fon-style-menu2' style={{visibility: showGrad2 ? "visible": "hidden"}}/> */}
+
+            <div style={{height: '100vh'}}>
                 <div className='form-edit-stavka'>
 
-                    <div className="edit-send" style={{ backgroundImage: `url(${enterSumma})`}}>
+                    {/* <div className="edit-send" style={{ backgroundImage: `url(${enterSumma})`}}>
                         <CurrencyInput 
                             placeholder='Впиши сюда сумму'
                             type="text"
                             value={summaStavki}
                             onChange={changeSummaStavki}  
                         />
-                    </div>
-        
-                    {/* <button onClick={sendStavka} className="button-send" style={{ backgroundImage: `url(${buttonSend})`}}>Отправить предложение</button> */}
+                    </div> */}
+                    
+                    <div className='rec1-input'></div>
+                    <div className='rec2-input'></div>
+                    <div className='rec3-input'></div>
+                        <input
+                            className='input-style3'
+                            placeholder='Впиши сюда сумму'
+                            type="text"
+                            value={summaStavki}
+                            onChange={changeSummaStavki} 
+                        /> 
+    
                 </div>
-            </div>
-
-            <div style={{position: 'fixed', bottom: '25px', right: '0'}}>
-                <img src={smallMenu} alt='' className='small-menu-icon' />
             </div>
 
         </div>
